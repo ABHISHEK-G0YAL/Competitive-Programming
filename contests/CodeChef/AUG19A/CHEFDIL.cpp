ca0a9cd04b6bf83763c466900538265cc124d0cc
Abhishek Goyal <abhishek252167@gmail.com>
2020-06-30 02:38:19 +0530

    added AUG19A

---
+++ contests/CodeChef/AUG19A/CHEFDIL.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/AUG19A/problems/CHEFDIL
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t, ones;
+    string s;
+    cin >> t;
+    while(t--) {
+        cin >> s;
+        ones = 0;
+        for(auto i : s) ones += (i - '0');
+        cout << (ones % 2 ? "WIN" : "LOSE") << endl;
+    }
+}
---
+++ contests/CodeChef/AUG19A/KS1.cpp
@@ -0,0 +1,30 @@
+// https://www.codechef.com/AUG19A/problems/KS1
+// Partially Solved (50pts)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for(int i = 0; i < n; i++)
+            cin >> a[i];
+        vector<pair<int, int> > parr;
+        for(int i = 0; i < n; i++) {
+            int xor_till_now = 0;
+            for(int k = i; k < n; k++) {
+                xor_till_now ^= a[k];
+                if(xor_till_now == 0)
+                    parr.push_back({i, k});
+            }
+        } int count = 0;
+        for(auto p : parr)
+            count += p.second - p.first;
+        cout << count << endl;
+    }
+}
