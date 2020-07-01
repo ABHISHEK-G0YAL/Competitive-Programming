e68f19b48f613e42e4ca1ce7e5268015d650ae0d
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-01 16:27:56 +0530

    merged problem description to py file

--- contests/foobar-2020/level_2/1_power-hungry.py
+++ contests/foobar-2020/level_2/1_power-hungry.py
@@ -1,3 +1,89 @@
+'''
+foobar:~/ abhishek252167$ request
+Requesting challenge...
+The latest gossip in the henchman breakroom is that "LAMBCHOP" stands for "Lambda's Anti-Matter Biofuel Collision Hadron Oxidating Potentiator". You're pretty sure it runs on diesel, not biofuel, but you can at least give the commander credit for trying.
+New challenge "Power Hungry" added to your home folder.
+Time to solve: 72 hours.
+foobar:~/ abhishek252167$ ls
+power-hungry
+journal.txt
+start_here.txt
+foobar:~/ abhishek252167$ cd power-hungry
+foobar:~/power-hungry abhishek252167$ ls
+Solution.java
+constraints.txt
+readme.txt
+solution.py
+foobar:~/power-hungry abhishek252167$ cat readme.txt
+Power Hungry
+============
+
+Commander Lambda's space station is HUGE. And huge space stations take a LOT of power. Huge space stations with doomsday devices take even more power. To help meet the station's power needs, Commander Lambda has installed solar panels on the station's outer surface. But the station sits in the middle of a quasar quantum flux field, which wreaks havoc on the solar panels. You and your team of henchmen have been assigned to repair the solar panels, but you'd rather not take down all of the panels at once if you can help it, since they do help power the space station and all!
+
+You need to figure out which sets of panels in any given array you can take offline to repair while still maintaining the maximum amount of power output per array, and to do THAT, you'll first need to figure out what the maximum output of each array actually is. Write a function solution(xs) that takes a list of integers representing the power output levels of each panel in an array, and returns the maximum product of some non-empty subset of those numbers. So for example, if an array contained panels with power output levels of [2, -3, 1, 0, -5], then the maximum product would be found by taking the subset: xs[0] = 2, xs[1] = -3, xs[4] = -5, giving the product 2*(-3)*(-5) = 30.  So solution([2,-3,1,0,-5]) will be "30".
+
+Each array of solar panels contains at least 1 and no more than 50 panels, and each panel will have a power output level whose absolute value is no greater than 1000 (some panels are malfunctioning so badly that they're draining energy, but you know a trick with the panels' wave stabilizer that lets you combine two negative-output panels to produce the positive output of the multiple of their power values). The final products may be very large, so give the solution as a string representation of the number.
+
+Languages
+=========
+
+To provide a Python solution, edit solution.py
+To provide a Java solution, edit Solution.java
+
+Test cases
+==========
+Your code should pass the following test cases.
+Note that it may also be run against hidden test cases not shown here.
+
+-- Python cases --
+Input:
+solution.solution([2, 0, 2, 2, 0])
+Output:
+    8
+
+Input:
+solution.solution([-2, -3, 4, -5])
+Output:
+    60
+
+-- Java cases --
+Input:
+Solution.solution({2, 0, 2, 2, 0})
+Output:
+    8
+
+Input:
+Solution.solution({-2, -3, 4, -5})
+Output:
+    60
+
+Use verify [file] to test your solution and see how it does. When you are finished editing your code, use submit [file] to submit your answer. If your solution passes the test cases, it will be removed from your home folder.
+
+
+foobar:~/power-hungry abhishek252167$ cat constraints.txt
+Java
+====
+Your code will be compiled using standard Java 8. All tests will be run by calling the solution() method inside the Solution class
+
+Execution time is limited.
+
+Wildcard imports and some specific classes are restricted (e.g. java.lang.ClassLoader). You will receive an error when you verify your solution if you have used a blacklisted class.
+
+Third-party libraries, input/output operations, spawning threads or processes and changes to the execution environment are not allowed.
+
+Your solution must be under 32000 characters in length including new lines and and other non-printing characters.
+
+Python
+======
+Your code will run inside a Python 2.7.13 sandbox. All tests will be run by calling the solution() function.
+
+Standard libraries are supported except for bz2, crypt, fcntl, mmap, pwd, pyexpat, select, signal, termios, thread, time, unicodedata, zipimport, zlib.
+
+Input/output operations are not allowed.
+
+Your solution must be under 32000 characters in length including new lines and and other non-printing characters.
+'''
+
 def solution(xs):
     nmax = -1001
     power = 1
