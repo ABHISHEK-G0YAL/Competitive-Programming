0a5d80f75e6ca2b05f4cbf07a0dbe3168f6129a4
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-08 19:11:56 +0530

    updated Maximal_Rectangle.cpp in LeetCode

--- practice/LeetCode/Maximal_Rectangle.cpp
+++ practice/LeetCode/Maximal_Rectangle.cpp
@@ -1,85 +1,66 @@
 // https://leetcode.com/problems/maximal-rectangle/
-// Accepted    116 ms
+// Accepted    112 ms
 
 class Solution {
 public:
-    vector<int>nsr(vector<int>heights){
-        vector<int>right;
-        stack<pair<int,int>>s;
-        int pseudoindex=heights.size();
-        int n=heights.size();
-        for(int i=n-1;i>=0;i--){
-            if(s.size()==0)
-                right.push_back(pseudoindex);
-            else if(s.size()>0 && s.top().first<heights[i])
-                right.push_back(s.top().second);
-            else if(s.size()>0 && s.top().first>=heights[i]) {
-                while(s.size()>0 && s.top().first>=heights[i])
+    vector<int> lefti(vector<int> &a) {
+        int n = a.size();
+        vector<int> v(n, -1);
+        stack<int> s;
+        s.push(0);
+        for(int i = 1; i < n; i++) {
+            if(a[s.top()] < a[i])
+                v[i] = s.top();
+            else {
+                while(!s.empty() and a[s.top()] >= a[i])
                     s.pop();
-                if(s.size()==0)
-                    right.push_back(pseudoindex);
-                else
-                    right.push_back(s.top().second);
+                if(!s.empty())
+                    v[i] = s.top();
             }
-            s.push({heights[i],i});
+            s.push(i);
         }
-        reverse(right.begin(),right.end());
-        return right;
+        return v;
     }
-    vector<int>nsl(vector<int> heights) {
-        vector<int>l;
-        stack<pair<int,int>>s;
-        int pseudoindex=-1;
-        int n=heights.size();
-        for(int i=0;i<n;i++){
-            if(s.size()==0)
-                l.push_back(pseudoindex);
-            else if(s.size()>0 && s.top().first<heights[i])
-                l.push_back(s.top().second);
-            else if(s.size()>0 && s.top().first>=heights[i]){
-                while(s.size()>0 && s.top().first>=heights[i])
+    vector<int> righti(vector<int> &a) {
+        int n = a.size();
+        vector<int> v(n, n);
+        stack<int> s;
+        s.push(n - 1);
+        for(int i = n - 2; i >= 0; i--) {
+            if(a[s.top()] < a[i])
+                v[i] = s.top();
+            else {
+                while(!s.empty() and a[s.top()] >= a[i])
                     s.pop();
-                if(s.size()==0)
-                    l.push_back(pseudoindex);
-                else
-                    l.push_back(s.top().second);
+                if(!s.empty())
+                    v[i] = s.top();
             }
-            s.push({heights[i],i});
+            s.push(i);
         }
-        return l;
+        return v;
     }
-    int mah(vector<int> heights) {
-        if(heights.size()==0)
-            return 0;
-        vector<int>left=nsl(heights);
-        vector<int>right=nsr(heights);
-        int n=heights.size();
-        vector<int>area;
-        for(int i=0;i<n;i++)
-            area.push_back(heights[i]*(right[i]-left[i]-1));
-        int maxi=INT_MIN;
-        for(int i=0;i<area.size();i++)
-            if(area[i]>maxi)
-                maxi=area[i];
-        return maxi; 
+    int largestRectangleAreaInHistogram(vector<int> &heights) {
+        int n = heights.size();
+        vector<int> i_left = lefti(heights);
+        vector<int> i_right = righti(heights);
+        long mx = 0;
+        for(int i = 0; i < n; i++)
+            mx = max(mx, heights[i] * (i_right[i] - i_left[i] - 1L));
+        return mx;
     }
-    int maximalRectangle(vector<vector<char>>& matrix){
-        if(matrix.size()==0)
+    int maximalRectangle(vector<vector<char>> &matrix){
+        if(matrix.empty())
             return 0;
         vector<int>v(matrix[0].size());
         int mx=INT_MIN;
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[i].size();j++){
-                if(i==0)
-                    v[j]=matrix[i][j]-'0';
-                else{
-                    if(matrix[i][j]=='0')
-                        v[j]=0;
-                    else
-                        v[j]=v[j]+1;
-                }
+                if(matrix[i][j]=='0')
+                    v[j]=0;
+                else
+                    v[j]+=1;
             }
-            mx=max(mx,mah(v));
+            mx=max(mx,largestRectangleAreaInHistogram(v));
         }
         return mx;
     }
