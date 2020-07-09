a108c7bcec94944d5503907cf07a163e27c53df0
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:19:54 +0530

    Added Arrays_Introduction.cpp from HackerRank

---
+++ practice/HackerRank/Arrays_Introduction.cpp
@@ -0,0 +1,18 @@
+// https://www.hackerrank.com/challenges/arrays-introduction/problem
+// Accepted
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    int n[1005];
+    for(int i=0;i<a;i++)
+    {
+        cin>>n[i];
+    }
+    for (int i=a-1;i>=0; i--) {
+        cout<<n[i]<<" ";
+    }
+}
