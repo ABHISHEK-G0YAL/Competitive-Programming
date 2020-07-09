01c1b01fbd2901f67c9ea5ae6685ff3ebcc0217b
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:34:29 +0530

    Added Mars_Exploration.cpp from HackerRank

---
+++ practice/HackerRank/Mars_Exploration.cpp
@@ -0,0 +1,32 @@
+// https://www.hackerrank.com/challenges/mars-exploration/problem
+// Accepted
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the marsExploration function below.
+int marsExploration(string s) {
+    string SOS="SOS";
+    int count=0;
+    for(int i=0;i<s.length();i++)
+        if(s[i]!=SOS[i%3])
+            ++count;
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = marsExploration(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
