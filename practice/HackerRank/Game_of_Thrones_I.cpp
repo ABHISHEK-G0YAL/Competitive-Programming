db94a32b93d22c1879225d73142121c0718abcc5
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:29:45 +0530

    Added Game_of_Thrones_I.cpp from HackerRank

---
+++ practice/HackerRank/Game_of_Thrones_I.cpp
@@ -0,0 +1,31 @@
+// https://www.hackerrank.com/challenges/game-of-thrones/problem
+// Accepted
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the gameOfThrones function below.
+string gameOfThrones(string s) {
+    int fre[26]={}; int cnt_nt_even=0;
+    for(int i=0;i<s.length();i++) fre[s[i]-'a']++;
+    for(int i=0;i<26;i++) cnt_nt_even+=(fre[i]%2);
+    if(cnt_nt_even<2) return "YES";
+    else return "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = gameOfThrones(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
