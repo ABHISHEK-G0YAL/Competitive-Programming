b1464cdd86ea43d94b35f71337eee6e57a5361e0
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:43:34 +0530

    Added Variable_Sized_Arrays.cpp from HackerRank

---
+++ practice/HackerRank/Variable_Sized_Arrays.cpp
@@ -0,0 +1,26 @@
+// https://www.hackerrank.com/challenges/variable-sized-arrays/problem
+// Accepted
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n,q,x,i,j;
+    cin>>n>>q;
+    int **varr=new int*[n];
+    for(i=0;i<n;i++) {
+        cin>>x;
+        *(varr+i)=new int[x];
+        for(j=0;j<x;j++)
+            cin>>*(*(varr+i)+j);
+    }
+    for(x=0;x<q;x++) {
+        cin>>i>>j;
+        cout<<*(*(varr+i)+j)<<endl;
+    }
+    return 0;
+}
+
