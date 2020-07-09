38570fdd2a4b99440f61f9f89d80b32b8a114a6c
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:39:12 +0530

    Added Security_Functions.cpp from HackerRank

---
+++ practice/HackerRank/Security_Functions.cpp
@@ -0,0 +1,28 @@
+// https://www.hackerrank.com/challenges/security-tutorial-functions/problem
+// Accepted
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the calculate function below.
+int calculate(int x) {
+    return x%11;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int x;
+    cin >> x;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = calculate(x);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
