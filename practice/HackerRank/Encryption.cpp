3bca7a742548f439dc3c7764138d6e92125b4beb
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:27:51 +0530

    Added Encryption.cpp from HackerRank

---
+++ practice/HackerRank/Encryption.cpp
@@ -0,0 +1,38 @@
+// https://www.hackerrank.com/challenges/encryption/problem
+// Accepted
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string encryption(string s) {
+    s.erase(remove(s.begin(),s.end(),' '),s.end());
+    int row=floor(sqrt(s.length())),col=ceil(sqrt(s.length()));
+    if(row*col<s.length());
+        row+=1;
+    char c[col][row+1];
+    memset(c,0,col*(row+1));
+    for(int i=0;i<row;i++)
+        for(int j=0;j<col;j++)
+            if(((i*col)+j)<s.length())
+                c[j][i]=s[(i*col)+j];
+    s.clear();
+    for(int j=0;j<col;j++)
+        s=s+c[j]+" ";
+    return s;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = encryption(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
