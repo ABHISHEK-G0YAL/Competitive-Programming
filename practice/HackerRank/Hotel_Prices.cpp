9ba8604e4ff9e1875261db59dbfc7dc7ce8a02f4
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:30:49 +0530

    Added Hotel_Prices.cpp from HackerRank

---
+++ practice/HackerRank/Hotel_Prices.cpp
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/challenges/hotel-prices/problem
+// Accepted
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class HotelRoom {
+    int bedrooms, bathrooms;
+public:
+    HotelRoom(int bedrooms, int bathrooms) : bedrooms(bedrooms), bathrooms(bathrooms) {}
+    virtual int get_price() {
+        return 50 * bedrooms + 100 * bathrooms;
+    }
+};
+
+class HotelApartment : public HotelRoom {
+public:
+    HotelApartment(int bedrooms, int bathrooms) : HotelRoom(bedrooms, bathrooms) {}
+    int get_price() {
+        return HotelRoom::get_price() + 100;
+    }
+};
+
