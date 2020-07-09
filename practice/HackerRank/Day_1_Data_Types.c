53959970741acd715b3410a6b9af61b40eb9b50e
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:25:44 +0530

    Added Day_1_Data_Types.c from HackerRank

---
+++ practice/HackerRank/Day_1_Data_Types.c
@@ -0,0 +1,19 @@
+// https://www.hackerrank.com/challenges/30-data-types/problem
+// Accepted
+
+    
+    // Declare second integer, double, and String variables.
+    int a;double b;char c[10000];
+    
+    // Read and save an integer, double, and String to your variables.
+    scanf("%d %lf %[^\n]s",&a,&b,c);
+    
+    // Print the sum of both integer variables on a new line.
+    printf("%d\n%.1lf\n%s%s",i+a,d+b,s,c);
+    
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    
+
