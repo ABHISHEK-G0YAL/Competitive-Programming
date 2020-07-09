55878c3327f2682ad1222d0504c22755cd45c442
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:37:45 +0530

    Added Preprocessor_Solution.cpp from HackerRank

---
+++ practice/HackerRank/Preprocessor_Solution.cpp
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/preprocessor-solution/problem
+// Accepted
+
+
+#define INF 1000000000
+#define FUNCTION(a,b)
+#define foreach(a, b) for(int i = 0; i < a.size(); i++)
+#define io(a) cin >> a
+#define minimum(a,b) a=a<b?a:b
+#define maximum(a,b) a=a>b?a:b
+#define toStr(a) #a
