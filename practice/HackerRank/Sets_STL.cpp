4a29eaa7508f6c3801ca83d4e3e64512673a39fe
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:39:47 +0530

    Added Sets_STL.cpp from HackerRank

---
+++ practice/HackerRank/Sets_STL.cpp
@@ -0,0 +1,30 @@
+// https://www.hackerrank.com/challenges/cpp-sets/problem
+// Accepted
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n,type,val;
+    cin >> n;
+    set<int> s;
+    while(n--) {
+        cin>>type>>val;
+        if(type==1) s.insert(val);
+        else if(type==2) s.erase(val);
+        else {
+            set<int>::iterator it=s.find(val);
+            if(it==s.end()) cout<<"No"<<endl;
+            else cout<<"Yes"<<endl;
+        }
+    }
+    return 0;
+}
+
+
+
