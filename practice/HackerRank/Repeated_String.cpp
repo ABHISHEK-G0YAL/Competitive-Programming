19c3b1ba070dff1fc38a2389c4df55b78b413010
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:38:36 +0530

    Added Repeated_String.cpp from HackerRank

---
+++ practice/HackerRank/Repeated_String.cpp
@@ -0,0 +1,41 @@
+// https://www.hackerrank.com/challenges/repeated-string/problem
+// Accepted
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the repeatedString function below.
+long repeatedString(string s, long n) {
+    long count_a=0,n_strings,part_s;
+    for(int i=0;i<s.length();i++)
+        if(s[i]=='a')
+            ++count_a;
+    n_strings=n/s.length();
+    part_s=n%s.length();
+    count_a*=n_strings;
+    for(int i=0;i<part_s;i++)
+        if(s[i]=='a')
+            ++count_a;
+    return count_a;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    long n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    long result = repeatedString(s, n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
