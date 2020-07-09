56b990ea6ca47987bdbc5805d3396704b91df6d3
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:20:31 +0530

    Added A_Very_Big_Sum.cpp from HackerRank

---
+++ practice/HackerRank/A_Very_Big_Sum.cpp
@@ -0,0 +1,26 @@
+// https://www.hackerrank.com/challenges/a-very-big-sum/problem
+// Accepted
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long aVeryBigSum(int n, vector <long> ar) {
+    // Complete this function
+    long sum=0;
+    for(int i=0;i<n;i++)
+        sum=ar[i]+sum;
+    return sum;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<long> ar(n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+       cin >> ar[ar_i];
+    }
+    long result = aVeryBigSum(n, ar);
+    cout << result << endl;
+    return 0;
+}
