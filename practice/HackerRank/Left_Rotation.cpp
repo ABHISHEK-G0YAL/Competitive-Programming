6a67a15609c7300b1b468bab7bb8aaaa43c3d541
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:32:28 +0530

    Added Left_Rotation.cpp from HackerRank

---
+++ practice/HackerRank/Left_Rotation.cpp
@@ -0,0 +1,19 @@
+// https://www.hackerrank.com/challenges/array-left-rotation/problem
+// Accepted
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,r;
+    cin>>n>>r;
+    int a[n];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    for(int i=0;i<n;i++)
+        cout<<a[(i+r)%n]<<" ";
+    return 0;
+}
+
