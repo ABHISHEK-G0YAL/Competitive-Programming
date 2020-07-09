1b71670f7ddb5e64ae3d0d94005c0eb970aa3a4a
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:41:24 +0530

    Added Summing_the_N_series.cpp from HackerRank

---
+++ practice/HackerRank/Summing_the_N_series.cpp
@@ -0,0 +1,41 @@
+// https://www.hackerrank.com/challenges/summing-the-n-series/problem
+// Accepted
+
+#include <bits/stdc++.h>
+#define mod 1000000007
+
+using namespace std;
+
+/*
+ * Complete the summingSeries function below.
+ */
+int summingSeries(long n) {
+    /*
+     * Write your code here.
+     */
+    return ((n%mod)*(n%mod))%mod;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        long n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        int result = summingSeries(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
