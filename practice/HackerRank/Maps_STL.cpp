89480c2405b0aca7141d7bc0f4438598bb089c40
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:34:07 +0530

    Added Maps_STL.cpp from HackerRank

---
+++ practice/HackerRank/Maps_STL.cpp
@@ -0,0 +1,30 @@
+// https://www.hackerrank.com/challenges/cpp-maps/problem
+// Accepted
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+
+int main() {
+    map<string,int> marksheet;
+    string name;
+    int n,type,marks;
+    cin>>n; while(n--) {
+        cin>>type;
+        if(type==1) {
+            cin>>name>>marks;
+            marksheet[name]+=marks;
+        } else if(type==2) {
+            cin>>name;
+            marksheet.erase(name);
+        } else {
+            cin>>name;
+            cout<<marksheet[name]<<endl;
+        }
+    } return 0;
+}
