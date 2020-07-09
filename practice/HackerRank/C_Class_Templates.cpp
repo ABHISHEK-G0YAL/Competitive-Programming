c6790a3f09477842f0842bac62c2de00565b39ba
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:24:58 +0530

    Added C_Class_Templates.cpp from HackerRank

---
+++ practice/HackerRank/C_Class_Templates.cpp
@@ -0,0 +1,16 @@
+// https://www.hackerrank.com/challenges/c-class-templates/problem
+// Accepted
+
+
+#define concatenate add
+#define cin ios_base::sync_with_stdio(false);cin.tie(NULL);cin
+template <typename T>
+class AddElements {
+    T element1;
+public:
+    AddElements (T arg) : element1(arg){
+    }
+    T add(T element2) {
+        return element1 + element2;
+    }
+};
