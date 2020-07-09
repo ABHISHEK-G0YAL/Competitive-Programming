be85b048ce616d68c77e537330c79b3c0aa96b61
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:26:48 +0530

    Added Day_9_Recursion_3.cpp from HackerRank

---
+++ practice/HackerRank/Day_9_Recursion_3.cpp
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/challenges/30-recursion/problem
+// Accepted
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int factorial(int n)
+{
+    if(n > 1)
+        return n * factorial(n - 1);
+    else
+        return 1;
+}
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin >> n;
+    cout << factorial(n);
+    return 0;
+}
