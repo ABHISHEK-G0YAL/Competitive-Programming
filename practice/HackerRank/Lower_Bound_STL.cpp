7520998b01e19335c575f89ea3017aa4a6050595
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:33:18 +0530

    Added Lower_Bound_STL.cpp from HackerRank

---
+++ practice/HackerRank/Lower_Bound_STL.cpp
@@ -0,0 +1,27 @@
+// https://www.hackerrank.com/challenges/cpp-lower-bound/problem
+// Accepted
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n,nq,q;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+        cin>>v[i]; 
+    cin>>nq;
+    for(int i=0;i<nq;i++) {
+        cin>>q;
+        vector<int>::iterator it;
+        it=lower_bound(v.begin(),v.end(),q);
+        if(*it==q) cout<<"Yes "<<it-v.begin()+1<<endl;
+        else cout<<"No "<<it-v.begin()+1<<endl;
+    }
+    return 0;
+}
