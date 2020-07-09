9d57d8139d68e2fb4a0d7e07d52592960d7da8f8
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:34:22 +0530

    Added Mark_and_Toys.cpp from HackerRank

---
+++ practice/HackerRank/Mark_and_Toys.cpp
@@ -0,0 +1,28 @@
+// https://www.hackerrank.com/challenges/mark-and-toys/problem
+// Accepted
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the maximumToys function below.
+int maximumToys(vector<int> &prices, int amount) {
+    sort(prices.begin(), prices.end());
+    int maxToys = 0;
+    while(amount >= prices[maxToys]) {
+        amount -= prices[maxToys];
+        maxToys += 1;
+    }
+    return maxToys;
+}
+
+int main() {
+    int n, amount;
+    cin >> n >> amount;
+    vector<int> prices(n);
+    for(int &price : prices)
+        cin >> price;
+    cout << maximumToys(prices, amount);
+}
