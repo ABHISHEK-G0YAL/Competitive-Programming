abc4fc9c08961aa8daddee5ff752df35d5fab015
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-09 18:24:51 +0530

    Added Cut_the_sticks.cpp from HackerRank

---
+++ practice/HackerRank/Cut_the_sticks.cpp
@@ -0,0 +1,94 @@
+// https://www.hackerrank.com/challenges/cut-the-sticks/problem
+// Accepted
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the cutTheSticks function below.
+vector<int> cutTheSticks(vector<int> arr)
+{
+    int n=arr.size(),i=0;
+    vector<int> sc;
+    sort(arr.begin(),arr.end());
+    while(arr[n-1])
+    {
+        while(arr[i]==0)
+            ++i;
+        sc.push_back(n-i);
+        int temp=arr[i];
+        for(int j=0;j<n;j++)
+            if(arr[j]!=0)
+                arr[j]-=temp;
+    }
+    return sc;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    vector<int> result = cutTheSticks(arr);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
