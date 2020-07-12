473caa95220673ff360fb26c37ec5357496b6a6e
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-13 00:29:45 +0530

    added Max Range Queries

---
+++ practice/codechef/MAXREMOV.cpp
@@ -0,0 +1,28 @@
+// https://www.codechef.com/COOK103A/problems/MAXREMOV
+// incomplete
+
+#include <bits/stdc++.h>
+#define MAX 100002
+using namespace std;
+int cake[MAX];
+
+int main() {
+    int t,n,k; cin>>t;
+    while(t--) {
+        memset(cake,0,sizeof(cake));
+        cin>>n>>k;
+        int l,r;
+        for(int i=0;i<n;i++) {
+            cin>>l>>r;
+            r+=1;
+            cake[l]+=1;
+            cake[r]-=1;
+        }
+        for(int i=1;i<MAX;i++)
+            cake[i]+=cake[i-1];
+        for(int i=1;i<11;i++)
+            cout<<cake[i]<<" ";
+        cout<<endl;
+        
+    }
+}
