fa789d9d30c0c8c33f91c96fb352882157c62e58
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-12 23:54:54 +0530

    fixed magic school from codechef

--- practice/codechef/CNTFAIL.cpp
+++ practice/codechef/CNTFAIL.cpp
@@ -1,25 +1,36 @@
 // https://www.codechef.com/problems/CNTFAIL
-// wrong answer
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int t, n, x;
+    int t, n, x, pass_c1, pass_c2, pass_max;
     cin >> t;
     while(t--) {
         cin >> n;
-        unordered_set<int> s;
+        unordered_map<int, int> m;
         for(int i = 0; i < n; i++) {
             cin >> x;
-            s.insert(x);
+            m[x] += 1;
         }
-        if(s.size() == 1 and *s.begin() == 0)
-            cout << n << endl;
-        else if(s.size() == 1 and *s.begin() == n - 1)
-            cout << 0 << endl;
-        else if(s.size() == 2 and abs(*s.begin() - *next(s.begin())) == 1 and max(*s.begin(), *next(s.begin())) <= n - 1)
-            cout << n - max(*s.begin(), *next(s.begin())) << endl;
+        if(m.size() == 1) {
+            pass_c1 = (*m.begin()).first;
+            if(pass_c1 == 0)
+                cout << n << endl;
+            else if(pass_c1 == n - 1)
+                cout << 0 << endl;
+            else
+                cout << -1 << endl;
+        }
+        else if(m.size() == 2) {
+            pass_c1 = (*m.begin()).first;
+            pass_c2 = (*next(m.begin())).first;
+            pass_max = max(pass_c1, pass_c2);
+            if(abs(pass_c2 - pass_c1) == 1 and pass_max <= n - 1 and m[pass_max] == n - pass_max)
+                cout << n - pass_max << endl;
+            else
+                cout << -1 << endl;
+        }
         else
             cout << -1 << endl;
     }
