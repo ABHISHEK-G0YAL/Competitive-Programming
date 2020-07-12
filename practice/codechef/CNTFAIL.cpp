defc698890aae6cb5717aaf5d0582ee6ed5602e6
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-12 23:50:44 +0530

    updated magic school

--- practice/codechef/CNTFAIL.cpp
+++ practice/codechef/CNTFAIL.cpp
@@ -1,34 +1,25 @@
 // https://www.codechef.com/problems/CNTFAIL
 // wrong answer
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int t,n;
-    cin>>t;
+    int t, n, x;
+    cin >> t;
     while(t--) {
-        cin>>n;
-        int x,a[n]={0},i1,i2;
-        for(int i=0;i<n;i++) {
-            cin>>x; ++a[x];
-        } x=0; bool fl=true; 
-        for(int i=0;i<n;i++) {
-            if(a[i]>0) {
-                ++x;
-                if(fl) {
-                    i1=i;
-                    fl=0;
-                }
-                else
-                    i2=i;
-            }
+        cin >> n;
+        unordered_set<int> s;
+        for(int i = 0; i < n; i++) {
+            cin>>x;
+            s.insert(x);
         }
-        if(x==1 && a[n-1]==n)
-            cout<<0<<endl;
-        else if(x==2 && i2-i1==1)
-            cout<<a[i2]<<endl;
+        if(s.size() > 2)
+            cout << -1 << endl;
+        else if(s.size() == 2) {
+            cout << n - max(*s.begin(), *next(s.begin())) << endl;
+        }
         else
-            cout<<-1<<endl;
+            cout << n - *s.begin() << endl;
     }
 }
