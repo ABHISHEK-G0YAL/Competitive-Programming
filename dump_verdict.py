7c5bcb8709a49e1131f5147bec4b2543e935d3fa
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-13 22:12:27 +0530

    updated verdict dump

--- dump_verdict.py
+++ dump_verdict.py
@@ -1,31 +1,31 @@
 import os
 import json
 
-# ulines = {}
-# exclude = {
-#     '.\practice': ['DSA', 'old-C-problems', 'Misc', '.jpeg', '.txt'],
-#     '.\contests': ['.html', '.pdf', 'Hash-Code', 'expedia_hiring_challenge_11Jan19', 'foobar_2020']
-# }
-# for root in exclude:
-#     for path, dirs, files in os.walk(root):
-#         for file in files:
-#             filepath = path + '\\' + file
-#             if any(x in filepath for x in exclude[root]):
-#                 continue
-#             with open(filepath) as f:
-#                 print(filepath)
-#                 f.readline()
-#                 line = f.readline()
-#                 if ulines.get(line):
-#                     ulines[line].append(filepath)
-#                 else:
-#                     ulines[line] = [filepath]
+ulines = {}
+exclude = {
+    '.\practice': ['DSA', 'old-C-problems', 'Misc', '.jpeg', '.txt'],
+    '.\contests': ['.html', '.pdf', 'Hash-Code', 'expedia_hiring_challenge_11Jan19', 'foobar_2020']
+}
+for root in exclude:
+    for path, dirs, files in os.walk(root):
+        for file in files:
+            filepath = path + '\\' + file
+            if any(x in filepath for x in exclude[root]):
+                continue
+            with open(filepath) as f:
+                print(filepath)
+                f.readline()
+                line = f.readline()
+                if ulines.get(line):
+                    ulines[line].append(filepath)
+                else:
+                    ulines[line] = [filepath]
 
-# with open('dump.json', 'w') as f:
-#     json.dump(ulines, f, indent=4)
+with open('dump.json', 'w') as f:
+    json.dump(ulines, f, indent=4)
 
-with open('dump.json') as f:
-    ulines = json.load(f)
+# with open('dump.json') as f:
+#     ulines = json.load(f)
 
 # del ulines['']
 
