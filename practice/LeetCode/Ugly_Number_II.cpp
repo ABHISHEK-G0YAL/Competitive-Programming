62c8f4375b15497b7c80771dac64aa24405f579e
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-28 14:16:01 +0530

    fixed Ugly_Number_II from LeetCode

--- practice/LeetCode/Ugly_Number_II.cpp
+++ practice/LeetCode/Ugly_Number_II.cpp
@@ -1,27 +1,20 @@
 // https://leetcode.com/problems/ugly-number-ii/
-// TLE
 
-#define N 1000000
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        bool arr[N] = {0, 1};
-        for(int i = 2; i < N / 2; i++) {
-            if(!arr[i]) {
-                for(int j = i; j < N; j += i) {
-                    if(i <= 5)
-                        arr[j] = true;
-                    else
-                        arr[j] = false;
-                }
-            }
+        int cnt[3] = {};
+        vector<int> v = {1};
+        while(v.size() < n){
+            int mi = min({v[cnt[0]] * 2, v[cnt[1]] * 3, v[cnt[2]] * 5});
+            if(mi == v[cnt[0]]*2)
+                ++cnt[0];
+            if(mi == v[cnt[1]]*3) 
+                ++cnt[1];
+            if(mi == v[cnt[2]]*5)
+                ++cnt[2];
+            v.push_back(mi);
         }
-        for(int i = 0; i < N; i++) {
-            if(arr[i])
-                un.push_back(i);
-        }
-        cout << un.size();
-        return un[n - 1];
+        return v.back();
     }
 };
-vector<int> Solution::un;
