bf095db88a575b7ccc1e65e16754b10efe6c9cd1
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-28 14:26:17 +0530

    made LeetCode Ugly_Number_II faster

--- practice/LeetCode/Ugly_Number_II.cpp
+++ practice/LeetCode/Ugly_Number_II.cpp
@@ -1,11 +1,12 @@
 // https://leetcode.com/problems/ugly-number-ii/
 
+int v[1700];
 class Solution {
 public:
     int nthUglyNumber(int n) {
         int cnt[3] = {};
-        vector<int> v = {1};
-        while(v.size() < n){
+        v[0] = 1;
+        for(int i = 1; i < n; ++i) {
             int mi = min({v[cnt[0]] * 2, v[cnt[1]] * 3, v[cnt[2]] * 5});
             if(mi == v[cnt[0]]*2)
                 ++cnt[0];
@@ -13,8 +14,8 @@ public:
                 ++cnt[1];
             if(mi == v[cnt[2]]*5)
                 ++cnt[2];
-            v.push_back(mi);
+            v[i] = mi;
         }
-        return v.back();
+        return v[n - 1];
     }
 };
