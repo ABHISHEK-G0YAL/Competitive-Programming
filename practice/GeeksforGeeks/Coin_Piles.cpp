275758ea4c97e6033753b7128f653d959a15785c
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-30 23:09:07 +0530

    Updated Coin_Piles from O(n^2) to O(n)

--- practice/GeeksforGeeks/Coin_Piles.cpp
+++ practice/GeeksforGeeks/Coin_Piles.cpp
@@ -1,35 +1,34 @@
 // https://practice.geeksforgeeks.org/problems/coin-piles/0/
 
-// O(n^2)
+// O(n)
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int t, n, k;
+    int t, n, k, x, sum, pile_cnt, pile_min[1005], pile_rem[1005];
     cin >> t;
     while(t--) {
         cin >> n >> k;
-        vector<int> v(n);
-        for(auto &i : v)
-            cin >> i;
-        sort(v.begin(), v.end());
-        int n_sum = 0, r_sum, max_pile, min_c = INT_MAX, i = 0;
-        while(i < n) {
-            r_sum = 0;
-            max_pile = v[i] + k;
-            for(int j = i; j < n; j++) {
-                if(v[j] - max_pile > 0)
-                    r_sum += v[j] - max_pile;
-            }
-            min_c = min(min_c, n_sum + r_sum);
-            while(i + 1 < n and v[i] == v[i + 1]) {
-                n_sum += v[i];
-                i += 1;
-            }
-            n_sum += v[i];
-            i += 1;
+        sum = 0;
+        memset(pile_min, 0, sizeof(pile_min));
+        memset(pile_rem, 0, sizeof(pile_rem));
+        for(int i = 0; i < n; i++) {
+            cin >> x;
+            sum += x;
+            ++pile_rem[x];
         }
+        pile_cnt = 0;
+        for(int i = 1; i < 1005; i++) {
+            pile_min[i] = n - pile_cnt;
+            pile_cnt += pile_rem[i];
+            pile_rem[i] *= i;
+            pile_min[i] += pile_min[i - 1];
+            pile_rem[i] += pile_rem[i - 1];
+        }
+        int min_c = INT_MAX;
+        for(int i = 1; i + k < 1005; i++)
+            min_c = min(min_c, sum - (pile_min[i + k] - pile_rem[i - 1]));
         cout << min_c << endl;
     }
 	return 0;
