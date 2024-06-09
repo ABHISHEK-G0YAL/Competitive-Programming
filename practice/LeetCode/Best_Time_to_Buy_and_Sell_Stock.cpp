a6e45db0782c39bb7a93d361a13bd2abe479564a
Abhishek Goyal <abhishek252167@gmail.com>
2024-06-10 00:11:52 +0530

    Updated LeetCode/Best_Time_to_Buy_and_Sell_Stock.cpp

--- practice/LeetCode/Best_Time_to_Buy_and_Sell_Stock.cpp
+++ practice/LeetCode/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -4,14 +4,10 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         const int n = prices.size();
-        vector<int> future_max(n);
-        future_max[n - 1] = prices[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            future_max[i] = max(prices[i], future_max[i + 1]);
-        }
-        int max_profit = 0;
-        for (int i = 0; i < n; i++) {
-            max_profit = max(max_profit, future_max[i] - prices[i]);
+        int future_max = 0, max_profit = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            future_max = max(prices[i], future_max);
+            max_profit = max(max_profit, future_max - prices[i]);
         }
         return max_profit;
     }
