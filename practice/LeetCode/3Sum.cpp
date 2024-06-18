286596526ab108a8bc143545a973e29110996efb
Abhishek Goyal <abhishek252167@gmail.com>
2024-06-18 18:33:55 +0530

    Updated LeetCode/3Sum.cpp

--- practice/LeetCode/3Sum.cpp
+++ practice/LeetCode/3Sum.cpp
@@ -4,29 +4,20 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        unordered_map<int, int> mp;
-        for (int &a : nums) {
-            ++mp[a];
-        }
         int n = nums.size();
         set<vector<int> > ans;
         for (int i = 0; i < n; i++) {
             if (i != 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            --mp[nums[i]];
+            int target = -nums[i];
+            unordered_map<int, int> indice_map;
             for (int j = i + 1; j < n; j++) {
-                --mp[nums[j]];
-                int to_search = -(nums[i] + nums[j]);
-                // cout << i << ": " << nums[i] << ", ";
-                // cout << j << ": " << nums[j] << ", ";
-                // cout << to_search << " " << mp[to_search]  << endl;
-                if (to_search >= nums[j] && mp[to_search] > 0) {
-                    ans.insert({nums[i], nums[j], to_search});
+                int c_i = indice_map[target - nums[j]];
+                if (c_i) {
+                    ans.insert({nums[i], nums[c_i - 1], nums[j]});
                 }
-            }
-            for (int j = i + 1; j < n; j++) {
-                ++mp[nums[j]];
+                indice_map[nums[j]] = j + 1;
             }
         }
         return vector<vector<int>>(ans.begin(), ans.end());
