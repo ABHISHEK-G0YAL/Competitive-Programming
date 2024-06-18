28518ea520db7c175fab9f6a5d3201700e0b659d
Abhishek Goyal <abhishek252167@gmail.com>
2024-06-18 18:34:58 +0530

    Updated LeetCode/3Sum.cpp

--- practice/LeetCode/3Sum.cpp
+++ practice/LeetCode/3Sum.cpp
@@ -5,21 +5,27 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        set<vector<int> > ans;
+        vector<vector<int>> ans;
         for (int i = 0; i < n; i++) {
             if (i != 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             int target = -nums[i];
-            unordered_map<int, int> indice_map;
-            for (int j = i + 1; j < n; j++) {
-                int c_i = indice_map[target - nums[j]];
-                if (c_i) {
-                    ans.insert({nums[i], nums[c_i - 1], nums[j]});
+            int j = i + 1, k = n - 1;
+            while(j < k) {
+                if (nums[j] + nums[k] > target) {
+                    --k;
+                } else if (nums[j] + nums[k] < target) {
+                    ++j;
+                } else {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    --k;
+                    ++j;
+                    while(j < n && nums[j - 1] == nums[j]) ++j;
+                    while(k >= 0 && nums[k] == nums[k + 1]) --k;
                 }
-                indice_map[nums[j]] = j + 1;
             }
         }
-        return vector<vector<int>>(ans.begin(), ans.end());
+        return ans;
     }
 };
