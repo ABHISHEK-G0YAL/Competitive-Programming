deda34907c9976546ae7ba06d5445a88abe2d6e4
Abhishek Goyal <abhishek252167@gmail.com>
2024-06-18 14:57:33 +0530

    Updated LeetCode/Maximum_Product_Subarray.cpp

--- practice/LeetCode/Maximum_Product_Subarray.cpp
+++ practice/LeetCode/Maximum_Product_Subarray.cpp
@@ -4,20 +4,16 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         const int n = nums.size();
-        double product = 1, maxProduct = INT_MIN;
+        double product1 = 1, product2 = 1, maxProduct = INT_MIN;
         for(int i = 0; i < n; i++) {
-            product *= nums[i];
-            maxProduct = max(product, maxProduct);
-            if (product == 0) {
-                product = 1;
+            product1 *= nums[i];
+            product2 *= nums[n - 1 - i];
+            maxProduct = max({product1, product2, maxProduct});
+            if (product1 == 0) {
+                product1 = 1;
             }
-        }
-        product = 1;
-        for(int i = n - 1; i >= 0; i--) {
-            product *= nums[i];
-            maxProduct = max(product, maxProduct);
-            if (product == 0) {
-                product = 1;
+            if (product2 == 0) {
+                product2 = 1;
             }
         }
         return maxProduct;
