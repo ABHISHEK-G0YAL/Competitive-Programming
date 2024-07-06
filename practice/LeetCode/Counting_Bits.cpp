40699d230d1702ac7d95fa8d32ac4c0127ffa47c
Abhishek Goyal <abhishek252167@gmail.com>
2024-07-06 20:09:29 +0530

    Updated LeetCode/Counting_Bits.cpp

--- practice/LeetCode/Counting_Bits.cpp
+++ practice/LeetCode/Counting_Bits.cpp
@@ -2,6 +2,17 @@
 
 class Solution {
 public:
+    // Brian Kernighan’s Algorithm
+    int optimisedGetBitCount(int n) {
+        int count = 0; 
+        while (n) {
+            n &= n - 1;
+            ++count;
+        }
+        return count;
+    }
+
+
     int getBitCount(int n) {
         // 1 << 31 is neagtive and causes problems
         // Use unsigned int or use 1 << 30
@@ -18,7 +29,14 @@ public:
     vector<int> countBits(int n) {
         vector<int> ans;
         for(int i = 0; i <= n; i++) {
-            ans.push_back(getBitCount(i));
+            // time complexity - O(b) where b is number of set bits
+            // ans.push_back(__builtin_popcount(i));
+
+            // time complexity - O(log n)
+            // ans.push_back(getBitCount(i));
+
+            // time complexity - O(b) where b is number of set bits
+            ans.push_back(optimisedGetBitCount(i));
         }
         return ans;
     }
