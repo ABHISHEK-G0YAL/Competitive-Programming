3afa40c12ed143f8da3c626152e6fe3ce5037620
Abhishek Goyal <abhishek252167@gmail.com>
2024-07-06 19:57:44 +0530

    Updated LeetCode/Counting_Bits.cpp

--- practice/LeetCode/Counting_Bits.cpp
+++ practice/LeetCode/Counting_Bits.cpp
@@ -2,10 +2,23 @@
 
 class Solution {
 public:
+    int getBitCount(int n) {
+        // 1 << 31 is neagtive and causes problems
+        // Use unsigned int or use 1 << 30
+        int mask = 1 << 30, count = 0;
+        while (mask) {
+            if (n & mask) {
+                ++count;
+            }
+            mask = mask >> 1;
+        }
+        return count;
+    }
+
     vector<int> countBits(int n) {
         vector<int> ans;
         for(int i = 0; i <= n; i++) {
-            ans.push_back(__builtin_popcount(i));
+            ans.push_back(getBitCount(i));
         }
         return ans;
     }
