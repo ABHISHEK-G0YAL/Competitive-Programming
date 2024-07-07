cd70b33f6563b0200169da88a7c457a702afa9ba
Abhishek Goyal <abhishek252167@gmail.com>
2024-07-07 13:10:57 +0530

    Update LeetCode/Linked_List_Cycle.cpp

--- practice/LeetCode/Linked_List_Cycle.cpp
+++ practice/LeetCode/Linked_List_Cycle.cpp
@@ -1,5 +1,3 @@
-// https://leetcode.com/problems/linked-list-cycle/
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -10,7 +8,7 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
+    bool hasCycleUsingHashMap(ListNode *head) {
         unordered_map<ListNode*, bool> visited;
         while(head) {
             if (visited.find(head) != visited.end()) {
@@ -21,5 +19,22 @@ public:
             head = head->next;
         }
         return false;
+    }
+
+    bool hasCycleUsingTwoPointer(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while(fast) {
+            slow = slow->next;
+            fast = fast->next ? fast->next->next : fast->next;
+            if (fast != nullptr && slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    bool hasCycle(ListNode *head) {
+        return hasCycleUsingTwoPointer(head);
     }
 };
