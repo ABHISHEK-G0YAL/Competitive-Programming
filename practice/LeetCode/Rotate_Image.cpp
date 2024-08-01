2385829cb78fbb2db2b74413c03cd5be9aca7639
Abhishek Goyal <abhishek252167@gmail.com>
2024-08-01 19:20:23 +0530

    Updated LeetCode/Rotate_Image.cpp

--- practice/LeetCode/Rotate_Image.cpp
+++ practice/LeetCode/Rotate_Image.cpp
@@ -5,28 +5,21 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size() - 1;
         for (int S = 0; S < matrix.size() / 2; ++S) {
-            int i = S, E = n - S, in, jn;
+            int i = S, E = n - S, io, jo;
             for (int j = S; j < E; ++j) {
-                cout << i << ", " << j << " | ";
-                in = S + E - j; jn = S;
-                swap(matrix[i][j], matrix[in][jn]);
+                io = i; jo = j;
                 i = S + E - j; j = S;
+                swap(matrix[io][jo], matrix[i][j]);
 
-                cout << i << ", " << j << " | ";
-                jn = i; in = E;
-                swap(matrix[i][j], matrix[in][jn]);
+                io = i; jo = j;
                 j = i; i = E;
+                swap(matrix[io][jo], matrix[i][j]);
 
-                cout << i << ", " << j << " | ";
-                in = S + E - j; jn = E;
-                swap(matrix[i][j], matrix[in][jn]);
+                io = i; jo = j;
                 i = S + E - j; j = E;
+                swap(matrix[io][jo], matrix[i][j]);
 
-                cout << i << ", " << j << " | ";
-                jn = i; in = S;
-                // swap(matrix[i][j], matrix[in][jn]);
                 j = i; i = S;
-
                 cout << endl;
             }
         }
