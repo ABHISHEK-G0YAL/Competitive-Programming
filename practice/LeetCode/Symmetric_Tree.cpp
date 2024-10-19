b94bddebae18d69ad9dcf01a30c66b690a06e130
Abhishek Goyal <abhishek252167@gmail.com>
2024-10-19 13:09:08 +0530

    Added LeetCode/Symmetric_Tree.cpp

---
+++ practice/LeetCode/Symmetric_Tree.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/symmetric-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root1, TreeNode* root2) {
+        if (root1 && root2) {
+            return root1->val == root2->val
+            && isSymmetric(root1->left, root2->right)
+            && isSymmetric(root1->right, root2->left);
+        }
+        return !root1 && !root2;
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if (!root) {
+            return true;
+        }
+        return isSymmetric(root->left, root->right);
+    }
+};
