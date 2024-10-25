26edc493fe455af5f765c1dbac2474078760a70c
Abhishek Goyal <abhishek252167@gmail.com>
2024-10-25 10:40:49 +0530

    Updated LeetCode/Edit_Distance.cpp

--- practice/LeetCode/Edit_Distance.cpp
+++ practice/LeetCode/Edit_Distance.cpp
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/edit-distance/
 
-class Solution {
+class SolutionOld {
 public:
     vector<vector<int> > mem=vector<vector<int> > (1000,vector<int> (1000,-1));
     int minDistance(string w1, string w2) {
@@ -16,5 +16,38 @@ public:
                           minDistance(w1.substr(1),w2),
                          minDistance(w1,w2.substr(1)) } );
         return mem[l1][l2];
+    }
+};
+
+class Solution {
+public:
+    // replacing recursive (i + 1, j + 1) -> dp[i - 1] [j - 1]
+    // inserting recursive (i, j + 1)-> dp[i] [j - 1]
+    // deleting recursive (i + 1, j) -> dp[i - 1] [j]
+    int editDistance(string &word1, string &word2) {
+        int n = word1.length(), m = word2.length();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int j = 0; j <= m; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + min({ dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j] });
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    int minDistance(string word1, string word2) {
+        return editDistance(word1, word2);
     }
 };
