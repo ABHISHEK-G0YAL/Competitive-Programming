c63775e2eeb4f77865e700e1acac9bb3bfe8ef02
Abhishek Goyal <abhishek252167@gmail.com>
2024-11-09 23:51:55 +0530

    Added InterviewBit/Meeting_rooms.cpp

---
+++ practice/InterviewBit/Meeting_rooms.cpp
@@ -0,0 +1,15 @@
+// https://www.interviewbit.com/problems/meeting-rooms/
+
+int Solution::solve(vector<vector<int> > &intervals) {
+    sort(intervals.begin(), intervals.end());
+    priority_queue<int, vector<int>, greater<int>> ongoingMeetings;
+    for (auto &currentMeeting : intervals) {
+        if (!ongoingMeetings.empty() && ongoingMeetings.top() <= currentMeeting[0]) {
+            ongoingMeetings.pop();
+            ongoingMeetings.push(currentMeeting[1]);
+        } else {
+            ongoingMeetings.push(currentMeeting[1]);
+        }
+    }
+    return ongoingMeetings.size();
+}
