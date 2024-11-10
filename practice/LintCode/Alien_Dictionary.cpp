230c34e85ecbf4c38a4bf88854603006def64909
Abhishek Goyal <abhishek252167@gmail.com>
2024-11-10 14:11:01 +0530

    Added LintCode/Alien_Dictionary.cpp

---
+++ practice/LintCode/Alien_Dictionary.cpp
@@ -0,0 +1,50 @@
+// https://www.lintcode.com/problem/892/
+
+class Solution {
+public:
+    string alienOrder(vector<string> &dictionary) {
+        unordered_map<char, unordered_set<char>> adj;
+        unordered_set<char> alphabet(dictionary[0].begin(), dictionary[0].end());
+        for (int i = 1; i < dictionary.size(); ++i) {
+            alphabet.insert(dictionary[i].begin(), dictionary[i].end());
+            int length = min(dictionary[i - 1].length(), dictionary[i].length());
+            bool allCharSame = true;
+            for (int j = 0; j < length; ++j) {
+                if (dictionary[i - 1][j] != dictionary[i][j]) {
+                    adj[dictionary[i - 1][j]].insert(dictionary[i][j]);
+                    allCharSame = false;
+                    break;
+                }
+            }
+            if (dictionary[i - 1].length() > dictionary[i].length() && allCharSame) {
+                return "";
+            }
+        }
+        unordered_map<char, int> indegree;
+        for (auto &[c, v] : adj) {
+            for (auto ch : v) {
+                ++indegree[ch];
+            }
+        }
+        // queue is changed to pq for lexicographically smallest topological sort
+        priority_queue<char, vector<char>, greater<char>> q;
+        for (char ch : alphabet) {
+            if (indegree[ch] == 0) {
+                q.push(ch);
+            }
+        }
+        string charOrder;
+        while (!q.empty()) {
+            char c = q.top();
+            q.pop();
+            for (auto ch : adj[c]) {
+                --indegree[ch];
+                if (indegree[ch] == 0) {
+                    q.push(ch);
+                }
+            }
+            charOrder += c;
+        }
+        return charOrder.length() == alphabet.size() ? charOrder : "";
+    }
+};
