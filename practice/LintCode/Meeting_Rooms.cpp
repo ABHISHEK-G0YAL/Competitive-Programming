37604817d2703d6174b85c419d1b878f48eec66a
Abhishek Goyal <abhishek252167@gmail.com>
2024-11-10 14:14:41 +0530

    Added LintCode/Meeting_Rooms.cpp

---
+++ practice/LintCode/Meeting_Rooms.cpp
@@ -0,0 +1,34 @@
+// https://www.lintcode.com/problem/920/
+
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+bool comprator(Interval &x, Interval &y) {
+    return x.end < y.end;
+}
+
+class Solution {
+public:
+    /**
+     * @param intervals: an array of meeting time intervals
+     * @return: if a person could attend all meetings
+     */
+    bool canAttendMeetings(vector<Interval> &intervals) {
+        sort(intervals.begin(), intervals.end(), comprator);
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i].start < intervals[i - 1].end) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
