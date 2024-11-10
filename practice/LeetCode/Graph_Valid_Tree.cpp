8a5bad54bce84b548860c4beacd49605ac6998c1
Abhishek Goyal <abhishek252167@gmail.com>
2024-11-10 17:29:15 +0530

    Added LeetCode/Graph_Valid_Tree.cpp

---
+++ practice/LeetCode/Graph_Valid_Tree.cpp
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/graph-valid-tree/
+// https://www.lintcode.com/problem/178/
+
+class Solution {
+public:
+    /**
+     * @param n: An integer
+     * @param edges: a list of undirected edges
+     * @return: true if it's a valid tree, or false
+     */
+
+    void dfs(vector<vector<int>> &adj, vector<bool> &visited, int v) {
+        visited[v] = true;
+        for (auto u : adj[v]) {
+            if (!visited[u]) {
+                dfs(adj, visited, u);
+            }
+        }
+    }
+
+    bool validTree(int n, vector<vector<int>> &edges) {
+        if (edges.size() != n - 1) {
+            return false;
+        }
+        vector<vector<int>> adj(n);
+        for (auto &edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+        vector<bool> visited(n);
+        dfs(adj, visited, 0);
+        for (auto visit : visited) {
+            if (!visit) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
