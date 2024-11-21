b025e63c2cd5c815612cf24c5bd3156e2a69114b
Abhishek Goyal <abhishek252167@gmail.com>
2024-11-22 03:41:07 +0530

    Fix WA_TLE/LeetCode/Couples_Holding_Hands.cpp

--- WA_TLE/practice/LeetCode/Couples_Holding_Hands.cpp
+++ WA_TLE/practice/LeetCode/Couples_Holding_Hands.cpp
@@ -1,20 +1,46 @@
 // https://leetcode.com/problems/couples-holding-hands/
-// Wrong Answer    N/A
 
 class Solution {
 public:
-    int minSwapsCouples(vector<int>& row) {
-        int n = row.size(), count = 0;
-        set<pair<int, int> > input;
-        for(int i = 0; i < n; i += 2)
-            input.insert({row[i], row[i + 1]});
-        for(int i = 0; i < n; i += 2) {
-            if(input.find({i, i + 1}) == input.end() and input.find({i + 1, i}) == input.end())
-                count += 1;
+    void dfs(unordered_map<int, vector<int>> &adj, int i, unordered_map<int, bool> &visited) {
+        visited[i] = true;
+        for (int v : adj[i]) {
+            if (!visited[v]) {
+                dfs(adj, v, visited);
+            }
         }
-        if(count == 0)
-            return 0;
-        else 
-            return count - 1;
+    }
+
+    int getMinSwaps(vector<pair<int,int>>& rowPair) {
+        unordered_map<int, vector<int>> adj;
+        unordered_map<int, bool> visited;
+        for (auto &[u, v] : rowPair) {
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+            visited[v] = false;
+            visited[u] = false;
+        }
+        int rings = 0;
+        for (auto &[i, vis] : visited) {
+            if (!vis) {
+                dfs(adj, i, visited);
+                ++rings;
+            }
+        }
+        return rowPair.size() - rings;
+    }
+
+    int minSwapsCouples(vector<int>& row) {
+        int n = row.size();
+        for (int &i : row) {
+            i = i / 2;
+        }
+        vector<pair<int, int>> rowPair;
+        for (int i = 0; i < n; i += 2) {
+            if (row[i] != row[i + 1]) {
+                rowPair.push_back({ row[i], row[i + 1] });
+            }
+        }
+        return getMinSwaps(rowPair);
     }
 };
