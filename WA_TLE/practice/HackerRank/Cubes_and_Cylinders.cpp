a2f7c52f2a8133eeba30fa5f0b43c43e40df3ff1
Abhishek Goyal <abhishek252167@gmail.com>
2024-11-22 04:46:52 +0530

    Fixed WA_TLE/HackerRank/Cubes_and_Cylinders.cpp

--- WA_TLE/practice/HackerRank/Cubes_and_Cylinders.cpp
+++ WA_TLE/practice/HackerRank/Cubes_and_Cylinders.cpp
@@ -1,40 +1,47 @@
 // https://www.hackerrank.com/contests/university-codesprint-4/challenges/cubes-and-cylinders/problem
-// Wrong Answer
 
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
-int main()
-{
-	int i,j,n,m,count=0;
-	cin >> n >> m;
-	int s[n],k[n],r[m],c[m];
-	for(i=0;i<n;i++)
-		cin>>s[i];
-	for(i=0;i<n;i++)
-		cin>>k[i];
-	for(i=0;i<m;i++)
-		cin>>r[i];
-	for(i=0;i<m;i++)
-		cin>>c[i];
-	i=-1;I:++i;if(i>=n) goto exit;
-	j=-1;J:++j;if(j>=m) goto I;
-	if(s[i]<=1.41421356237F*r[j] && c[j]!=0)
-	{
-		while(k[i]!=0 && c[j]!=0)
-		{  
-			k[i]=k[i]-1;
-			c[j]=c[j]-1;
-			++count;   
-		}   
-	}
-	if(k[i]==0 && c[j]==0)
-	goto I;
-	if(k[i]==0)
-	goto I;
-	if(c[j]==0)
-	goto J;
-	exit:;
-	cout<<count;
-	return 0;
-}	
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<pair<int, int>> cubes(n), cylinders(m);
+    for (auto &[s, k] : cubes) {
+        cin >> s;
+    }
+    for (auto &[s, k] : cubes) {
+        cin >> k;
+    }
+    for (auto &[r, c] : cylinders) {
+        cin >> r;
+    }
+    for (auto &[r, c] : cylinders) {
+        cin >> c;
+    }
+    sort(cubes.begin(), cubes.end());
+    sort(cylinders.begin(), cylinders.end());
+    int totalStoredPackages = 0;
+    for (int i = 0, j = 0; i < n && j < m;) {
+        auto &[cubeEdge, numberCubes] = cubes[i];
+        auto &[cylinderRadius, maxPackages] = cylinders[j];
+        if (1.4142 * cubeEdge < 2 * cylinderRadius) {
+            int storedPackages = min(numberCubes, maxPackages);
+            totalStoredPackages += storedPackages;
+            numberCubes -= storedPackages;
+            maxPackages -= storedPackages;
+            if (numberCubes == 0) {
+                ++i;
+            }
+            if (maxPackages == 0) {
+                ++j;
+            }
+        } else {
+            ++j;
+        }
+    }
+    cout << totalStoredPackages;
+}
