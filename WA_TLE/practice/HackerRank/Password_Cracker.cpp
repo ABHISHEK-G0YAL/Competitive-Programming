8acd81e88c661364a8813cd0f167900e6f4fbc05
Abhishek Goyal <abhishek252167@gmail.com>
2024-11-24 01:11:08 +0530

    Fixed WA_TLE/HackerRank/Password_Cracker.cpp  - TLE -> AC

--- WA_TLE/practice/HackerRank/Password_Cracker.cpp
+++ WA_TLE/practice/HackerRank/Password_Cracker.cpp
@@ -1,29 +1,33 @@
 // https://www.hackerrank.com/challenges/password-cracker/problem
-// Time Limit Exceded
 
 #include <bits/stdc++.h>
 using namespace std;
 
+unordered_map<string_view, string> memo;
 string checkPassword(unordered_set<string_view> &pass_set, string_view loginAttempt) {
     if (loginAttempt.empty()) {
         return "";
+    }
+    if (memo.find(loginAttempt) != memo.end()) {
+        return memo[loginAttempt];
     }
     string verified;
     for (int i = 1; i <= loginAttempt.size(); i++) {
         if (pass_set.find(loginAttempt.substr(0, i)) != pass_set.end()) {
             verified = checkPassword(pass_set, loginAttempt.substr(i));
             if (verified != "WRONG PASSWORD") {
-                return verified.empty() ?
+                return memo[loginAttempt] = verified.empty() ?
                     string(loginAttempt.substr(0, i)) :
                     string(loginAttempt.substr(0, i)) + " " + verified;
             }
         }
     }
-    return "WRONG PASSWORD";
+    return memo[loginAttempt] = "WRONG PASSWORD";
 }
 
 string checkPassword(vector<string> &passwords, string loginAttempt) {
     unordered_set<string_view> pass_set(passwords.begin(), passwords.end());
+    memo.clear();
     return checkPassword(pass_set, loginAttempt);
 }
 
