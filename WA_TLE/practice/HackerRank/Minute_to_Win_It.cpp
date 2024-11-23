f8b11cedb0f1bfe284d376266c37c8fba51ea04a
Abhishek Goyal <abhishek252167@gmail.com>
2024-11-23 22:52:14 +0530

    Fixed WA_TLE/HackerRank/Minute_to_Win_It.cpp

--- WA_TLE/practice/HackerRank/Minute_to_Win_It.cpp
+++ WA_TLE/practice/HackerRank/Minute_to_Win_It.cpp
@@ -1,36 +1,18 @@
 // https://www.hackerrank.com/contests/w38/challenges/minute-to-win-it/problem
-// Wrong Answer
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int minuteToWinIt(vector<long> a, int k) {
-    int n = a.size();
-    int pos = 0, count = 0, max = 0;
-    for (int i = 1; i < n; i++) {
-        if (a[i] - a[i - 1] == k)
-            ++count;
-        else
-            count = 0;
-        if (count > max) {
-            pos = i;
-            max = count;
-        }
+int minuteToWinIt(vector<long> a, long k) {
+    // a`[i] = C + i * k;
+    unordered_map<long, int> f_mp;
+    int max_correct = 0;
+    for (int i = 0; i < a.size(); i++) {
+        long C = a[i] - i * k;
+        ++f_mp[C];
+        max_correct = max(max_correct, f_mp[C]);
     }
-    int time = 0;
-    for (int i = pos - max; i > 0; i--) {
-        if (a[i] - a[i - 1] != k) {
-            a[i - 1] = a[i] - k;
-            ++time;
-        }
-    }
-    for (int i = pos; i < n - 1; i++) {
-        if (a[i + 1] - a[i] != k) {
-            a[i + 1] = a[i] + k;
-            ++time;
-        }
-    }
-    return time;
+    return a.size() - max_correct;
 }
 
 int main() {
