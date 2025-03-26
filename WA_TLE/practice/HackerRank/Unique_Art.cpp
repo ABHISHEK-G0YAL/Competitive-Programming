861462ca8c70f0e89595b1fec624590f77b2c40c
Abhishek Goyal <abhishek252167@gmail.com>
2025-03-26 16:42:02 +0530

    Updated WA_TLE/practice/HackerRank/Unique_Art.cpp

--- WA_TLE/practice/HackerRank/Unique_Art.cpp
+++ WA_TLE/practice/HackerRank/Unique_Art.cpp
@@ -2,11 +2,7 @@
 // Terminated due to timeout
 
 #include <bits/stdc++.h>
-
 using namespace std;
-
-vector<string> split_string(string);
-
 
 /*
  * Complete the student function below.
@@ -24,72 +20,18 @@ int student(int begin,int end,vector<int> t) {
 }
 
 
-int main()
-{
+int main() {
     int n;
     cin >> n;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    string t_temp_temp;
-    getline(cin, t_temp_temp);
-
-    vector<string> t_temp = split_string(t_temp_temp);
-
     vector<int> t(n);
-
-    for (int t_itr = 0; t_itr < n; t_itr++) {
-        int t_item = stoi(t_temp[t_itr]);
-
-        t[t_itr] = t_item;
+    for (int &i : t) {
+        cin >> i;
     }
-
-    int q;
+    int q, l, r;
     cin >> q;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    for (int q_itr = 0; q_itr < q; q_itr++) {
-        string ij_temp;
-        getline(cin, ij_temp);
-
-        vector<string> ij = split_string(ij_temp);
-
-        int i = stoi(ij[0]);
-
-        int j = stoi(ij[1]);
-
-        int result = student(i, j, t);
-
-        cout << result << "\n";
+    while (q--) {
+        cin >> l >> r;
+        cout << student(l, r, t) << "\n";
     }
-
     return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
 }
