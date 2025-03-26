d78a309b87f1436d30b2fc84e49921073af31c80
Abhishek Goyal <abhishek252167@gmail.com>
2025-03-26 20:07:02 +0530

    Updated WA_TLE/HackerRank/Akhil_and_GF.cpp

--- WA_TLE/practice/HackerRank/Akhil_and_GF.cpp
+++ WA_TLE/practice/HackerRank/Akhil_and_GF.cpp
@@ -2,10 +2,7 @@
 // Wrong Answer
 
 #include <bits/stdc++.h>
-
 using namespace std;
-
-vector<string> split_string(string);
 
 // Complete the solve function below.
 int solve(long n, int m) {
@@ -31,59 +28,15 @@ int solve(long n, int m) {
         return loop[(n - initial.size() - 1) % loop.size()];
 }
 
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
+int main() {
     int t;
     cin >> t;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    for (int t_itr = 0; t_itr < t; t_itr++) {
-        string nm_temp;
-        getline(cin, nm_temp);
-
-        vector<string> nm = split_string(nm_temp);
-
-        long n = stol(nm[0]);
-
-        int m = stoi(nm[1]);
-
-        int result = solve(n, m);
-
-        fout << result << "\n";
+    while (t--) {
+        long n;
+        int m;
+        cin >> n >> m;
+        cout << solve(n, m) << "\n";
     }
-
-    fout.close();
-
     return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
 }
