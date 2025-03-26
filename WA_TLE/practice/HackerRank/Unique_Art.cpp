716e2af8ef0390d41b094d29baef5caf7b5dea4f
Abhishek Goyal <abhishek252167@gmail.com>
2025-03-26 19:58:07 +0530

    Fixed WA_TLE/practice/HackerRank/Unique_Art.cpp

--- WA_TLE/practice/HackerRank/Unique_Art.cpp
+++ WA_TLE/practice/HackerRank/Unique_Art.cpp
@@ -1,37 +1,114 @@
 // https://www.hackerrank.com/contests/university-codesprint-4/challenges/unique-art/problem
-// Terminated due to timeout
 
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
- * Complete the student function below.
- */
-int student(int begin,int end,vector<int> t) {
-    // Return the number of paintings that this student will appreciate.
-    unordered_map<int, int> hm;
-    int i,j,c=0;
-    for(i=begin-1;i<end;i++)
-    hm[t[i]]++;
-    for(i=begin-1;i<end;i++)
-        if(hm[t[i]]==1)
-            ++c;
-    return c;
+struct Query {
+    int l, r, idx;
+};
+
+int BLOCK_SIZE;
+
+bool cmpQuery(const Query &a, const Query &b) {
+    int blockA = a.l / BLOCK_SIZE;
+    int blockB = b.l / BLOCK_SIZE;
+    if(blockA == blockB)
+        return a.r < b.r;
+    return blockA < blockB;
 }
 
-
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     int n;
     cin >> n;
-    vector<int> t(n);
-    for (int &i : t) {
-        cin >> i;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
     }
-    int q, l, r;
+    
+    // Coordinate compression: compress values to [0, M-1]
+    vector<int> vals = arr;
+    sort(vals.begin(), vals.end());
+    vals.erase(unique(vals.begin(), vals.end()), vals.end());
+    for (int i = 0; i < n; i++){
+        arr[i] = lower_bound(vals.begin(), vals.end(), arr[i]) - vals.begin();
+    }
+    int m = vals.size();  // number of distinct painting values
+    
+    int q;
     cin >> q;
-    while (q--) {
+    vector<Query> queries(q);
+    for (int i = 0; i < q; i++){
+        int l, r;
         cin >> l >> r;
-        cout << student(l, r, t) << "\n";
+        // Convert from 1-indexed to 0-indexed.
+        queries[i] = {l - 1, r - 1, i};
     }
+    
+    // Determine block size (approximately sqrt(n))
+    BLOCK_SIZE = static_cast<int>(sqrt(n));
+    
+    sort(queries.begin(), queries.end(), cmpQuery);
+    
+    // Frequency array for painting values (after compression)
+    vector<int> freq(m, 0);
+    int currL = 0, currR = -1, currAns = 0;
+    vector<int> ans(q, 0);
+    
+    auto add = [&](int idx) {
+        int val = arr[idx];
+        freq[val]++;
+        if(freq[val] == 1) {
+            // Newly added unique element
+            currAns++;
+        } else if(freq[val] == 2) {
+            // It was unique and now appears twice
+            currAns--;
+        }
+    };
+    
+    auto remove = [&](int idx) {
+        int val = arr[idx];
+        freq[val]--;
+        if(freq[val] == 1) {
+            // After removal, becomes unique
+            currAns++;
+        } else if(freq[val] == 0) {
+            // Element removed completely from window
+            currAns--;
+        }
+    };
+    
+    // Process queries in Mo's order.
+    for (auto &query : queries) {
+        int L = query.l, R = query.r;
+        
+        // Expand the current range [currL, currR] to [L, R]
+        while (currR < R) {
+            currR++;
+            add(currR);
+        }
+        while (currR > R) {
+            remove(currR);
+            currR--;
+        }
+        while (currL < L) {
+            remove(currL);
+            currL++;
+        }
+        while (currL > L) {
+            currL--;
+            add(currL);
+        }
+        
+        ans[query.idx] = currAns;
+    }
+    
+    for (int res : ans) {
+        cout << res << "\n";
+    }
+    
     return 0;
 }
