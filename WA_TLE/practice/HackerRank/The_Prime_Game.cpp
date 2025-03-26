1f450bf2d95a6b4b20110343c2ca54a34c236c4c
Abhishek Goyal <abhishek252167@gmail.com>
2025-03-26 15:38:30 +0530

    Fixed WA_TLE/HackerRank/The_Prime_Game.cpp  - WA -> AC

--- WA_TLE/practice/HackerRank/The_Prime_Game.cpp
+++ WA_TLE/practice/HackerRank/The_Prime_Game.cpp
@@ -1,79 +1,30 @@
 // https://www.hackerrank.com/challenges/the-prime-game/problem
-// Wrong Answer
-
 
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<string> split_string(string);
+// vector<int> primeMoves = { 2, 3, 5, 7, 11, 13 };
+vector<int> grundyCycle = {0, 0, 1, 1, 2, 2, 3, 3, 4};
 
-// Complete the winner function below.
-string winner(vector<int> A) {
-    // Return the name of the winner of the game
-    return "Manasa";
+int computeGrundy(long long n) {
+    return grundyCycle[n % 9];  // Use precomputed cycle
 }
 
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    int t;
-    cin >> t;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    for (int t_itr = 0; t_itr < t; t_itr++) {
-        int n;
-        cin >> n;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-        string A_temp_temp;
-        getline(cin, A_temp_temp);
-
-        vector<string> A_temp = split_string(A_temp_temp);
-
-        vector<int> A(n);
-
-        for (int i = 0; i < n; i++) {
-            int A_item = stoi(A_temp[i]);
-
-            A[i] = A_item;
+int main() {
+    int T, K;
+    cin >> T;
+    long balls;
+    while (T--) {
+        cin >> K;
+        int xorSum = 0;
+        for (int i = 0; i < K; i++) {
+            cin >> balls;
+            xorSum ^= computeGrundy(balls);
         }
-
-        string result = winner(A);
-
-        fout << result << "\n";
+        if (xorSum == 0)
+            cout << "Sandy\n";
+        else
+            cout << "Manasa\n";
     }
-
-    fout.close();
-
     return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
 }
