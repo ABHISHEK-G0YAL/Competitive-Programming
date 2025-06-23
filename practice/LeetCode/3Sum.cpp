5199731729a4d9854a4e9d1dd9656b32b4bb0a3e
Abhishek Goyal <abhishekgoya@adobe.com>
2025-06-23 16:22:28 +0530

    Update LeetCode/3Sum.cpp

--- practice/LeetCode/3Sum.cpp
+++ practice/LeetCode/3Sum.cpp
@@ -2,30 +2,31 @@
 
 class Solution {
 public:
+    vector<vector<int>> triplets;
+    void twoSum(vector<int>& nums, int l, int r, int target) {
+        int sum, n = nums.size();
+        while (l < r) {
+            sum = nums[l] + nums[r];
+            if (sum > target) {
+                --r;
+            } else if (sum < target) {
+                ++l;
+            } else {
+                triplets.push_back({ -target, nums[l], nums[r] });
+                while(r > 0 && nums[r] == nums[r - 1]) --r; --r;
+                while(l < n - 1 && nums[l] == nums[l + 1]) ++l; ++l;
+            }
+        }
+    }
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        vector<vector<int>> ans;
         for (int i = 0; i < n; i++) {
-            if (i != 0 && nums[i] == nums[i - 1]) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int target = -nums[i];
-            int j = i + 1, k = n - 1;
-            while(j < k) {
-                if (nums[j] + nums[k] > target) {
-                    --k;
-                } else if (nums[j] + nums[k] < target) {
-                    ++j;
-                } else {
-                    ans.push_back({nums[i], nums[j], nums[k]});
-                    --k;
-                    ++j;
-                    while(j < n && nums[j - 1] == nums[j]) ++j;
-                    while(k >= 0 && nums[k] == nums[k + 1]) --k;
-                }
-            }
+            twoSum(nums, i + 1, n - 1, -nums[i]);
         }
-        return ans;
+        return triplets;
     }
 };
