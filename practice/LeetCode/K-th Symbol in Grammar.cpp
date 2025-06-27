85aae60d81f1a3320945a3d3cdc43b46626eb617
Abhishek Goyal <abhishek252167@gmail.com>
2025-06-28 01:21:17 +0530

    Update LeetCode K-th Symbol in Grammar

--- practice/LeetCode/K-th Symbol in Grammar.cpp
+++ practice/LeetCode/K-th Symbol in Grammar.cpp
@@ -2,10 +2,15 @@
 
 class Solution {
 public:
-    int kthGrammar(int N, int K) {
-        if(K == 1) return 0;
-        bool sa = kthGrammar(N, (K + 1) / 2);
-        if(K & 1) return sa;
-        else return !sa;
+    int kthGrammar(int n, int k) {
+        if (n == 1) {
+            return 0;
+        }
+        int subProblem = kthGrammar(n - 1, (k + 1) / 2);
+
+        // if k is odd, first child is ans
+        // 0 -> 01, 1 -> 10, first child = subproblem, second child = !subproblem
+        return k % 2 ? subProblem : !subProblem;
+
     }
 };
