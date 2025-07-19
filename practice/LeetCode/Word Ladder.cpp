9a81a60e02e66c3684baec8b03ba0605896b4e47
Abhishek Goyal <abhishek252167@gmail.com>
2025-07-19 12:05:57 +0530

    Update Word Ladder solution to improve BFS implementation and optimize adjacency list creation using pattern

--- practice/LeetCode/Word Ladder.cpp
+++ practice/LeetCode/Word Ladder.cpp
@@ -38,8 +38,11 @@ public:
     }
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_map<string, vector<string>> adj;
+        // O(N^2 L)
         for (int i = 0; i < wordList.size(); i++) {
+            // O(N L)
             for (int j = i + 1; j < wordList.size(); j++) {
+                // O(L)
                 if (isOneCharDiff(wordList[i], wordList[j])) {
                     adj[wordList[i]].push_back(wordList[j]);
                     adj[wordList[j]].push_back(wordList[i]);
@@ -58,5 +61,55 @@ public:
             return 0;
         }
         return bfs(adj, beginWord, endWord);
+    }
+};
+
+class Solution {
+public:
+    int bfs(unordered_map<string, vector<string>> &patternMap, string startWord, string endWord) {
+        int count = 1;
+        queue<vector<string>> q;
+        unordered_set<string> visited;
+        q.push({startWord});
+        visited.insert(startWord);
+        // O (N L^2)
+        while (!q.empty()) {
+            ++count;
+            vector<string> level;
+            for (auto &parentWord : q.front()) {
+                // O(L^2)
+                for (int i = 0; i < parentWord.length(); i++) {
+                    string pattern(parentWord);  // O(L)
+                    pattern[i] = '*';
+                    for (string &childWord : patternMap[pattern]) {
+                        if (childWord == endWord) {
+                            return count;
+                        }
+                        if (visited.count(childWord) == 0) {
+                            level.push_back(childWord);
+                            visited.insert(childWord);
+                        }
+                    }
+                }
+            }
+            if (!level.empty()) {
+                q.push(level);
+            }
+            q.pop();
+        }
+        return 0;
+    }
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string, vector<string>> patternMap;
+        // O(N L^2)
+        for (string &word : wordList) {
+            // O(L^2) (Can be optimised to O(L))
+            for (int i = 0; i < word.length(); i++) {
+                string pattern(word); // O(L)
+                pattern[i] = '*';
+                patternMap[pattern].push_back(word);
+            }
+        }
+        return bfs(patternMap, beginWord, endWord);
     }
 };
